e0d34e7adbf0116cb35d4387f5c8733c82eb6c6e
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2016-10-21 11:33:08 -0400

    Modify shear simulation code to generate ABC samples

--- src/simulate_shear.py
+++ src/simulate_shear.py
@@ -1,96 +1,147 @@
-# stuff we import
-import numpy as np
-from lenstools.simulations import Nicaea
+"""Generates ABC draws of shear maps from the prior distribution"""
+
+import random
+import sys
 import galsim
-import treecorr
+from lenstools.simulations import Nicaea
+import numpy as np
 
-# Global variables (for shame)
-grid_nx = 100
-# length of grid in one dimension (degrees)
-theta = 10.
-# grid spacing
-dtheta = theta/grid_nx
-nbins = 10
-# parameters for corr2:
-min_sep = dtheta # lowest bin starts at grid spacing
-max_sep = grid_nx * np.sqrt(2) * dtheta # upper edge of upper bin is at maximum pair separation
+class Constants:
+    def __init__(self, H0, OmegaM, Ode0, sigma8, Ob0):
+        self.H0 = H0
+        self.OmegaM = OmegaM
+        self.Ode0 = Ode0
+        self.sigma8 = sigma8
+        self.Ob0 = Ob0
 
-## Stolen from Rachel
-def run_treecorr(x, y, g1, g2):
-    """Helper routine to take outputs of GalSim shear grid routine, and run treecorr on it."""
-    import pyfits
-    import os
-    import treecorr
-    # Use fits binary table for faster I/O.
-    assert x.shape == y.shape
-    assert x.shape == g1.shape
-    assert x.shape == g2.shape
-    x_col = pyfits.Column(name='x', format='1D', array=x.flatten() )
-    y_col = pyfits.Column(name='y', format='1D', array=y.flatten() )
-    g1_col = pyfits.Column(name='g1', format='1D', array=g1.flatten() )
-    g2_col = pyfits.Column(name='g2', format='1D', array=g2.flatten() )
-    cols = pyfits.ColDefs([x_col, y_col, g1_col, g2_col])
-    table = pyfits.new_table(cols)
-    phdu = pyfits.PrimaryHDU()
-    hdus = pyfits.HDUList([phdu,table])
-    hdus.writeto('temp.fits',clobber=True)
-    # Define the treecorr catalog object.
-    #     cat = treecorr.Catalog('temp.fits',x_units='degrees',y_units='degrees', x_col='x',y_col='y',g1_col='g1',g2_col='g2')
-    cat = treecorr.Catalog('temp.fits',x_units='degrees',y_units='degrees', x_col='x',y_col='y',g1_col='g1',g2_col='g2')
-    # Define the corrfunc object
-    ### WAS: gg = treecorr.GGCorrelation(min_sep=min_sep, max_sep=max_sep, bin_size=0.1, sep_units='degrees')
-    gg = treecorr.GGCorrelation(min_sep=min_sep, max_sep=max_sep, nbins = nbins, sep_units='degrees', bin_slop = 0.2)
-    # Actually calculate the correlation function.
-    gg.process(cat)
-    os.remove('temp.fits')
-    return gg
+    def nicaea_object(self):
+        return Nicaea(H0=self.H0, Om0=self.OmegaM, Ode0=self.Ode0, sigma8=self.sigma8, Ob0=self.Ob0)
 
-## ABC lensing simulations
-# FOR LATER: It would be good if we had all of this stored in a config file and then had a program that read those all in.
+    def write_to_file(self, fname):
+        np.savetxt(fname, np.array([self.H0, self.OmegaM,
+                                    self.Ode0,
+                                    self.sigma8,
+                                    self.Ob0]))
 
-## Big picture: ideally this is a module, which you could, in principle, swap out for a much more sophisticated black box
-## WE STILL NEED TO FIGURE OUT HOW TO ADD SHAPE NOISE APPROPRIATELY TO THESE ESTIMATES
-## SENSIBLE THING SEEMS TO BE TO PASS IN S/N AS A PARAMETERg
-## Function simulateShear
 
-# Takes cosmological parameters, generates a shear map, adds noise, computes binned CF summary statistics and returns them...
-# Inputs (for now): OmegaM_arg, sigma8_arg, noise_arg, seed
-# if seed_arg = 0 (the default), then it should use "a random seed from the system" per the documentation
-# noise_arg is, for now, just the standard deviation for the IID Gaussian
-def simulate_shear(OmegaM_arg, sigma8_arg, noise_arg, seed_arg = 0):
-    
-    ## Parameters that are fixed, for now (i.e., the prior is a Dirac delta function!)
-    H0Fix = 70.0
-    Ob0Fix = 0.045
-    z_use = 0.7
-    
-    # wavenumbers to evaluate power spectra at
-    ell_use = np.logspace(-2.0, 4.0, num = 50)
+def draw_cosmological_constants(H0=None, OmegaM=None, Ode0=None, sigma8=None, Ob0=None):
+  """Draws a set of cosmological constants from the prior distributions
 
-    # Force that Om0 + Ode0 = 1
-    nicaeaObs = Nicaea(H0 = H0Fix, Om0 = OmegaM_arg, Ode0 = 1-OmegaM_arg, sigma8 = sigma8_arg, Ob0=Ob0Fix)
-    psObs_nicaea = nicaeaObs.convergencePowerSpectrum(ell = ell_use, z = z_use)
-    psObs_tabulated = galsim.LookupTable(ell_use, psObs_nicaea, interpolant = 'linear')
-    ps_galsim = galsim.PowerSpectrum(psObs_tabulated, delta2 = False, units = galsim.radians)
-    
-    grid_deviate = galsim.BaseDeviate(seed_arg)
-    #    g1, g2, kappa = ps_galsim.buildGrid(grid_spacing=dtheta, ngrid=grid_nx, rng=grid_deviate, units='degrees', kmin_factor=2, kmax_factor=2)
-    g1, g2, kappa = ps_galsim.buildGrid(grid_spacing=dtheta, ngrid=grid_nx, rng=grid_deviate, units='degrees', kmin_factor=2, kmax_factor=2, get_convergence = True)
-    g1_r, g2_r, mu = galsim.lensing_ps.theoryToObserved(g1, g2, kappa)
-    rng = galsim.GaussianDeviate(lseed = seed_arg)
+  Inputs:
+    H0: float, a cosmological constant
+    OmegaM: float, a cosmological constant
+    Ode0: float, a cosmological constant
+    sigma8: float, a cosmological constant
+    Ob0: float, a cosmological constant
 
-    g1_noise_grid = galsim.ImageD(grid_nx, grid_nx)
-    g2_noise_grid = galsim.ImageD(grid_nx, grid_nx)
-    g1_noise_grid.addNoise(galsim.GaussianNoise(rng = rng, sigma = noise_arg))
-    g2_noise_grid.addNoise(galsim.GaussianNoise(rng = rng, sigma = noise_arg))
-    
-    g1_noisy = np.add(g1_r, g1_noise_grid.array)
-    g2_noisy = np.add(g2_r, g2_noise_grid.array)
+  Returns:
+    A NICAEA object with the constants set.
+  """
 
-    grid_range = dtheta * np.arange(grid_nx)
-    x, y = np.meshgrid(grid_range, grid_range)
-    gg = run_treecorr(x, y, g1_noisy, g2_noisy)
-    
-    to_return = {'log_r' : gg.logr, 'xipm' : np.hstack((gg.xip,gg.xim))}
-    #    to_return = np.hstack((gg.xip,gg.xim))
-    return(to_return)
+  if H0 is None:
+    H0 = 70.0
+  if OmegaM is None:
+    OmegaM = 0.2
+  if Ode0 is None:
+    Ode0 = 1 - OmegaM # Force that Om0 + Ode0 = 1
+  if sigma8 is None:
+    sigma8 = random.uniform(0, OmegaM)
+  if Ob0 is None:
+    Ob0 = 0.045
+
+  return Constants(H0=H0, OmegaM=OmegaM, Ode0=Ode0, sigma8=sigma8, Ob0=Ob0)
+
+
+def simulate_shear(constants=None, noise_sd=0.0, seed=0):
+  """Takes cosmological parameters, generates a shear map, and adds
+  noise.
+
+  Inputs:
+    OmegaM: float, cosmological constant
+    sigma8: float, cosmological constant
+    noise_sd: float, the standard deviation for IID Gaussian noise.
+    seed: integer, seed for the RNG; if 0 it uses a randomly chosen seed.
+
+  Returns:
+    A pair of shear spectrum grids.
+  """
+
+  grid_nx = 100
+  # length of grid in one dimension (degrees)
+  theta = 10.
+  # grid spacing
+  dtheta = theta/grid_nx
+
+  redshift = 0.7
+
+  # wavenumbers at which to evaluate power spectra
+  ell = np.logspace(-2.0, 4.0, num=50)
+
+  if constants is None:
+      constants = draw_cosmological_constants()
+  nicaea_obj = constants.nicaea_object()
+  psObs_nicaea = nicaea_obj.convergencePowerSpectrum(ell=ell, z=redshift)
+  psObs_tabulated = galsim.LookupTable(ell, psObs_nicaea, interpolant='linear')
+  ps_galsim = galsim.PowerSpectrum(psObs_tabulated, delta2=False, units=galsim.radians)
+
+  grid_deviate = galsim.BaseDeviate(seed)
+  g1, g2, kappa = ps_galsim.buildGrid(grid_spacing=dtheta,
+                                      ngrid=grid_nx,
+                                      rng=grid_deviate,
+                                      units='degrees',
+                                      kmin_factor=2, kmax_factor=2,
+                                      get_convergence=True)
+  g1_r, g2_r, _ = galsim.lensing_ps.theoryToObserved(g1, g2, kappa)
+  rng = galsim.GaussianDeviate(seed=seed)
+
+  g1_noise_grid = galsim.ImageD(grid_nx, grid_nx)
+  g2_noise_grid = galsim.ImageD(grid_nx, grid_nx)
+
+  g1_noise_grid.addNoise(galsim.GaussianNoise(rng=rng, sigma=noise_sd))
+  g2_noise_grid.addNoise(galsim.GaussianNoise(rng=rng, sigma=noise_sd))
+
+  g1_noisy = np.add(g1_r, g1_noise_grid.array)
+  g2_noisy = np.add(g2_r, g2_noise_grid.array)
+
+  return g1_noisy, g2_noisy
+
+def main(outdir, true_constants, noise_sd, ndraws, epsilon):
+  """Simulates shear images using ABC.
+
+  Inputs:
+    outfile: string, the file to write out the images.
+    true_constants: NICAEA object, the "true" cosmological constants.
+    noise_sd: float, the standard deviation of added IID Gaussian noise.
+    ndraws: int, the number of draws from the posterior distribution.
+    epsilon: float, the threshold for ABC selection.
+
+  Side Effects:
+    Populates outdir with files g1-1.csv, g2-1.csv,
+    g1-2.csv, and so on.
+  """
+  true_g1, true_g2 = simulate_shear(true_constants, noise_sd=0.0, seed=1)
+
+  true_constants.write_to_file(outdir + "/params-0.csv")
+  np.savetxt(outdir + "/g1-0.csv", true_g1)
+  np.savetxt(outdir + "/g2-0.csv", true_g2)
+
+  for ii in xrange(ndraws):
+    print "Iteration {}\n".format(ii)
+    while True:
+      draw = draw_cosmological_constants()
+      g1, g2 = simulate_shear(draw, seed=ii)
+      if np.linalg.norm(true_g1 - g1) < epsilon and np.linalg.norm(true_g2 - g2) < epsilon:
+        draw.write_to_file(outdir + "/constants-{}.csv".format(ii + 1))
+        np.savetxt(outdir + "/g1-{}.csv".format(ii + 1), g1)
+        np.savetxt(outdir + "/g2-{}.csv".format(ii + 1), g2)
+        break
+
+
+if __name__ == '__main__':
+  outdir = sys.argv[1]
+  true_constants = draw_cosmological_constants(sigma8=0.2)
+  noise_sd = 0.0
+  ndraws = int(sys.argv[2])
+  epsilon = float(sys.argv[3])
+
+  main(outdir, true_constants, noise_sd, ndraws, epsilon)
