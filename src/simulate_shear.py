ef6152729667b687fbb23a110e6b6af2fe7bba9e
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2017-10-12 08:45:00 -0400

    Syntax changes

--- src/simulate_shear.py
+++ src/simulate_shear.py
@@ -1,48 +1,59 @@
 """Generates ABC draws of shear maps from the prior distribution"""
 
+import os
+import random
+import sys
+
 import galsim
 import h5py
 from lenstools.simulations import Nicaea
 import numpy as np
-import random
-import sys
+import pyfits
 import treecorr
+
 
 class Constants:
     """Class for cosmological constants"""
-    def __init__(self, H0, OmegaM, Ode0, sigma8, Ob0):
+    def __init__(self, H0, omega_m, Ode0, sigma_8, Ob0):
         self.H0 = H0
-        self.OmegaM = OmegaM
+        self.omega_m = omega_m
         self.Ode0 = Ode0
-        self.sigma8 = sigma8
+        self.sigma_8 = sigma_8
         self.Ob0 = Ob0
 
     def nicaea_object(self):
-        return Nicaea(H0=self.H0, Om0=self.OmegaM, Ode0=self.Ode0, sigma8=self.sigma8, Ob0=self.Ob0)
+        return Nicaea(H0=self.H0, Om0=self.omega_m, Ode0=self.Ode0,
+                      sigma8=self.sigma_8, Ob0=self.Ob0)
 
-import pyfits
-import numpy as np
-import os
-import treecorr
+    def write_to_hdf5(self, hdf_file):
+        hdf_file.create_dataset('constants', data=np.array([self.H0,
+                                                            self.omega_m,
+                                                            self.Ode0,
+                                                            self.sigma_8,
+                                                            self.Ob0]))
 
-def run_treecorr(x, y, g1, g2, min_sep, max_sep, nbins = 10):
+def run_treecorr(x, y, g1, g2, min_sep, max_sep, nbins=10):
     """Run treecorr on GalSim shear grid routine"""
-    # Use fits binary table for faster I/O.
+
     assert x.shape == y.shape
     assert x.shape == g1.shape
     assert x.shape == g2.shape
-    x_col = pyfits.Column(name='x', format='1D', array=x.flatten() )
-    y_col = pyfits.Column(name='y', format='1D', array=y.flatten() )
-    g1_col = pyfits.Column(name='g1', format='1D', array=g1.flatten() )
-    g2_col = pyfits.Column(name='g2', format='1D', array=g2.flatten() )
+
+    x_col = pyfits.Column(name='x', format='1D', array=x.flatten())
+    y_col = pyfits.Column(name='y', format='1D', array=y.flatten())
+    g1_col = pyfits.Column(name='g1', format='1D', array=g1.flatten())
+    g2_col = pyfits.Column(name='g2', format='1D', array=g2.flatten())
     cols = pyfits.ColDefs([x_col, y_col, g1_col, g2_col])
     table = pyfits.new_table(cols)
     phdu = pyfits.PrimaryHDU()
-    hdus = pyfits.HDUList([phdu,table])
-    hdus.writeto('temp.fits',clobber=True)
+    hdus = pyfits.HDUList([phdu, table])
+    hdus.writeto('temp.fits', clobber=True)
+
     # Define the treecorr catalog object.
-    cat = treecorr.Catalog('temp.fits',x_units='degrees',y_units='degrees', x_col='x',y_col='y',g1_col='g1',g2_col='g2')
-    gg = treecorr.GGCorrelation(min_sep=min_sep, max_sep=max_sep, nbins = nbins, sep_units='degrees', bin_slop = 0.2)
+    cat = treecorr.Catalog('temp.fits', x_units='degrees', y_units='degrees',
+                           x_col='x', y_col='y', g1_col='g1', g2_col='g2')
+    gg = treecorr.GGCorrelation(min_sep=min_sep, max_sep=max_sep, nbins=nbins,
+                                sep_units='degrees', bin_slop=0.2)
     gg.process(cat)
     os.remove('temp.fits')
 
@@ -64,11 +75,9 @@ def simulate_shear(constants, redshift, noise_sd=0.0, seed=0):
         A pair of shear spectrum grids.
     """
 
-    grid_nx = 100
-    # length of grid in one dimension (degrees)
-    theta = 10.0
-    # grid spacing
-    dtheta = theta/grid_nx
+    grid_nx = 100 # length of grid in one dimension (degrees)
+    theta = 10.0 # grid spacing
+    dtheta = theta / grid_nx
 
     # wavenumbers at which to evaluate power spectra
     ell = np.logspace(-2.0, 4.0, num=50)
@@ -101,8 +110,8 @@ def simulate_shear(constants, redshift, noise_sd=0.0, seed=0):
     max_sep = grid_nx * np.sqrt(2) * dtheta
     grid_range = dtheta * np.arange(grid_nx)
     x, y = np.meshgrid(grid_range, grid_range)
-    stats = run_treecorr(x, y, g1, g2, min_sep, max_sep)    
-    
+    stats = run_treecorr(x, y, g1, g2, min_sep, max_sep)
+
     return g1_noisy, g2_noisy, stats
 
 def main(outdir, true_constants, redshift, ndraws, noise_sd):
@@ -121,41 +130,39 @@ def main(outdir, true_constants, redshift, ndraws, noise_sd):
     g1, g2, stats = simulate_shear(true_constants, redshift, noise_sd=noise_sd, seed=0)
     write_to_file(outdir + "draw-0.hdf5", true_constants, g1, g2, stats)
 
-    for ii in range(1, ndraws + 1):
-        fname = outdir + "draw-{}.hdf5".format(ii)
+    for sim in range(1, ndraws + 1):
+        fname = outdir + "draw-{}.hdf5".format(sim)
         constants = draw_constants()
-        g1, g2, stats = simulate_shear(constants, redshift, noise_sd=noise_sd, seed=ii)
+        g1, g2, stats = simulate_shear(constants, redshift, noise_sd=noise_sd, seed=sim)
 
-        
         write_to_file(fname, constants, g1, g2, stats)
-        
+
 
 def write_to_file(fname, constants, g1, g2, stats):
-    h5f = h5py.File(fname, "w")
+    hdf_file = h5py.File(fname, "w")
 
-    h5f.create_dataset('g1', data = g1)
-    h5f.create_dataset('g2', data = g2)
-    h5f.create_dataset('constants', data = np.array([constants.H0, constants.OmegaM,
-                                                     constants.Ode0, constants.sigma8,
-                                                     constants.Ob0]))
-    h5f.create_dataset('log_r', data = stats['log_r'])
-    h5f.create_dataset('xipm', data = stats['xipm'])
-    
-    h5f.close()
+    constants.write_to_hdf5(hdf_file)
+
+    hdf_file.create_dataset('g1', data=g1)
+    hdf_file.create_dataset('g2', data=g2)
+    hdf_file.create_dataset('log_r', data=stats['log_r'])
+    hdf_file.create_dataset('xipm', data=stats['xipm'])
+
+    hdf_file.close()
 
 def draw_constants():
     """ Draws cosmological constants from prior distribution"""
-    
-    OmegaM = random.uniform(0.1, 0.8)
-    sigma8 = random.uniform(0.5, 1.0)
 
-    return Constants(H0=0.70, OmegaM=OmegaM, Ode0 = 0.697,
-                     sigma8=sigma8, Ob0 = 0.045)
+    omega_m = random.uniform(0.1, 0.8)
+    sigma_8 = random.uniform(0.5, 1.0)
+
+    return Constants(H0=0.70, omega_m=omega_m, Ode0=0.697,
+                     sigma_8=sigma_8, Ob0=0.045)
 
 if __name__ == '__main__':
     outdir = sys.argv[1]
-    true_constants = Constants(H0=0.70, OmegaM=0.25, Ode0 = 0.697,
-                               sigma8=0.8, Ob0 = 0.045)
+    true_constants = Constants(H0=0.70, omega_m=0.25, Ode0 = 0.697,
+                               sigma_8=0.8, Ob0 = 0.045)
     noise_sd = 0.00811
     redshift = 0.7
     ndraws = int(sys.argv[2])
